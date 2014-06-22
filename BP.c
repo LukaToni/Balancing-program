5f41441a286af6b8cd1048fbde41afa63041193b
Luka <luka.toni93@gmail.com>
2014-06-22 12:26:31 +0200

    Create BP.c

---
+++ BP.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXLINE 100
+
+int main()
+{
+	double balance = 0.0;
+	char line1[MAXLINE], line2[MAXLINE];
+
+	while(gets(line1) > 0)
+    {
+        gets(line2);
+
+        if(strcmp(line1, "deposit") == 0)
+            balance += atof(line2);
+        else if(strcmp(line1, "check") == 0)
+            balance -= atof(line2);
+        else
+        {
+            printf("bad data line: not \"check\" or \"deposit\"\n");
+            continue;
+        }
+
+        printf("balance: %.2f\n", balance);
+    }
+	return 0;
+}
