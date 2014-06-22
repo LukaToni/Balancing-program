16901de71cf8c059ed0b5ea608999d1a1ba062c4
Luka <luka.toni93@gmail.com>
2014-06-22 12:45:36 +0200

    Update BP.c

--- BP.c
+++ BP.c
@@ -9,20 +9,20 @@ int main()
 	char line1[MAXLINE], line2[MAXLINE];
 
 	while(gets(line1) > 0)
-    {
-        gets(line2);
-
-        if(strcmp(line1, "deposit") == 0)
-            balance += atof(line2);
-        else if(strcmp(line1, "check") == 0)
-            balance -= atof(line2);
-        else
-        {
-            printf("bad data line: not \"check\" or \"deposit\"\n");
-            continue;
-        }
-
-        printf("balance: %.2f\n", balance);
-    }
+	{
+		gets(line2);
+		
+		if(strcmp(line1, "deposit") == 0)
+		    balance += atof(line2);
+		else if(strcmp(line1, "check") == 0)
+		    balance -= atof(line2);
+		else
+		{
+		    printf("bad data line: not \"check\" or \"deposit\"\n");
+		    continue;
+		}
+		
+		printf("balance: %.2f\n", balance);
+	}
 	return 0;
 }
